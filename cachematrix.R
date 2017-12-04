a29d5c8ee2ffdf41bf9509d5783f5090a0a999ef
iymangler <31528725+iymangler@users.noreply.github.com>
2017-12-04 00:16:31 -0500

    First commit post trial

--- cachematrix.R
+++ cachematrix.R
@@ -4,7 +4,17 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+   myMatrix<-NULL
+   
+   setMatrixVal<-function(y) {
+     x<<-y
+     myMatrix<<-NULL
+   }
+   
+   getMatrixVal<-function() x 
+   setInverseMatrix<-function(inverse) myMatrix<<-inverse
+   getInverseMatrix<-function() myMatrix
+   list(setMatrixVal=setMatrixVal, getMatrixVal=getMatrixVal, setInverseMatrix=setInverseMatrix, getInverseMatrix)
 }
 
 
